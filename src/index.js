06a60da067a9cab22a26dc054622cd85047e4b2b
1zemzem <1zemliakova@gmail.com>
2023-01-27 18:11:10 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if (str.length % 2) {
+    return false;
+  }
+
+  let brackets = {};
+  let stack = [];
+
+  for (let el of bracketsConfig) {
+    let open = el[1];
+    let close = el[0];
+    brackets[open] = close;
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    if (
+      brackets[str[i]] === stack[stack.length - 1] &&
+      brackets[str[i]] != undefined
+    )
+      stack.pop();
+    else stack.push(str[i]);
+  }
+  if (stack.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
